14dc11e1025eadcc9152ecba52ca9a1f5118797e
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-04-24 16:56:28 -0300

    salvando

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+  pensamento = {
+    id: '1',
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert("Novo pensamento criar");
+  }
+
+  cancelarPensamento() {
+    alert("Pensamento cancelado");
+  }
+
+}
