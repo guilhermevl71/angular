2e997581d95dc457178c7f99fdff5bb35cb96541
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-05-02 14:08:44 -0300

    'aaaa'

--- src/app/componentes/pensamentos/excluir-pensamneto/excluir-pensamneto.component.ts
+++ src/app/componentes/pensamentos/excluir-pensamneto/excluir-pensamneto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-excluir-pensamneto',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./excluir-pensamneto.component.css']
 })
 export class ExcluirPensamnetoComponent implements OnInit {
-
-  constructor() { }
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+  constructor(private service: PensamentoService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt (id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento
+    })
+  }
+
+  excluirPensamento() {
+    if(this.pensamento.id){
+
+    }
   }
 
 }
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -20,8 +20,13 @@ export class PensamentoService {
     return this.http.post<Pensamento>(this.API, pensamento)
   }
 
-  excluirPensamento(id: number): Observable<Pensamento> {
-    const url = `${this.API}/${id}` /* é preciso desse const url pq para deletar é atraves do id, e a url da API é a base com tudo no caso, então é preciso ter pensamentos/id e pra isso cria a url com isso, daria pra colocar ja direto dentro do delete */
+  excluir(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}` /* é preciso desse const url pq para deletar é atraves do id, e a url da API é a base com tudo no caso, então é preciso ter localhost:3000/pensamentos/id e pra isso cria a url com isso, daria pra colocar ja direto dentro do delete */
     return this.http.delete<Pensamento>(url)
+  }
+
+  buscarPorId(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}`
+    return this.http.get<Pensamento>(url)
   }
 }
